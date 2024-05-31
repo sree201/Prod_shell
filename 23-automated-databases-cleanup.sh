ac2fc48fae8668119108464515e78c4d245ac4f8
Sree201 <sreesirinath4@gmail.com>
2024-05-30 20:26:18 -0700

    other-script

--- 23-automated-databases-cleanup.sh
+++ 23-automated-databases-cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-database_name="your_database"
+database_name="mysql"
 
 days_to_keep=7
 
-find /path/to/database/backups -name "$database_name*.sql" -mtime +"$days_to_keep" -exec rm {} \;
+find /path/to/mysql/backups -name "$database_name*.sql" -mtime +"$days_to_keep" -exec rm {} \;
 
 echo "Old database backups cleaned up."
