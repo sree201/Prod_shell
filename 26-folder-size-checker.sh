7539fe06901f3eaa80c4f3f024ebc984caad66f9
Sree201 <sreesirinath4@gmail.com>
2024-05-30 20:34:29 -0700

    other-script

--- 26-folder-size-checker.sh
+++ 26-folder-size-checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-folder_path="/home/ec2-user/shell-expense/backend.service /etc/systemd/system/backend.service/"
+folder_path="/home/ec2-user/shell-expense/backend.service /etc/systemd/system/backend.service"
 
 du -sh "$folder_path"
 
