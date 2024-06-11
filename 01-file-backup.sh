5820aefad4e567b4ceabc42180d21236ffcc86f5
Sree201 <sreesirinath4@gmail.com>
2024-06-11 00:47:20 -0700

    backend

--- 01-file-backup.sh
+++ 01-file-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-backup_dir="/path/to/backup"
+backup_dir="/tmp/backup-app-logs"
 
-source_dir="/path/to/source"
+source_dir="/tmp/app-logs"
 
 tar -czf "$backup_dir/backup_$(date +%Y%m%d_%H%M%S).tar.gz" "$source_dir"
